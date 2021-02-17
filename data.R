8ac23da73c395acad7cccf56d54bff5f865ac34e
MaximeBalan <79017648+MaximeBalan@users.noreply.github.com>
2021-02-17 12:53:38 +0100

    Update data.R

--- data.R
+++ data.R
@@ -3,31 +3,33 @@ library(dplyr)
 library(ggpubr)
 getwd()
 setwd(dir = "D:/STAGE")
-data <- read.csv("interface_app.jold_ll_trial.2.csv")
-dist <- abs(data$init_dist - data$end_dist)
-dist
 
-data <- cbind(data,dist)
-data
-score <- (data$dist + data$time_trial)
-data <- cbind(data,score)
-data
-??ggqqplot
-plot(data$trial,data$score)
+## Import data
+data <- read.csv("pre_data.csv")
 
-ggqqplot(data, x = "score", facet.by = "session")
-ggplot(data, aes(x = trial,y=score), facet.by = "session")+
-  geom_bar(position="stack", stat="identity")
+## Score at each trail sort by participant
+qplot(data = data, y = score, x = trial, geom = c("line", "point"),xlim = c(0, 58)) + 
+  facet_wrap(~ participant)
 
-ggplot()+
-  geom_point(data,aes(x=trial, y=score))
-?aes
-?ggplot
-?geom_bar
-session1 <- subset(data, session =="jold_ll.1.0")
-session2 <- subset(data, session =="jold_ll.2.0")
-session3 <- subset(data, session =="jold_ll.5.0")
+## Score mean of each participant
+data %>%
+  group_by(participant) %>%
+  get_summary_stats(score, type = "mean_sd")
 
-ggplot(data, aes(fill=trial, y=score, x=session)) + 
-  geom_bar(position="dodge", stat="identity")
+## Score with 2*time_trial
+qplot(data = data, y = scoret2, x = trial, geom = c("line", "point"),xlim = c(0, 58)) + 
+  facet_wrap(~ participant)
+
+data %>%
+  group_by(participant) %>%
+  get_summary_stats(scoret2, type = "mean_sd")
+
+## Score with 10*time_trial
+qplot(data = data, y = scoret3, x = trial, geom = c("line", "point"),xlim = c(0, 58)) + 
+  facet_wrap(~ participant)
+
+data %>%
+  group_by(participant) %>%
+  get_summary_stats(scoret3, type = "mean_sd")
+ 
 
