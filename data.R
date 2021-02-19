be10154dcc5a7fefa22af52bd6fff1a869bf27c1
MaximeBalan <79017648+MaximeBalan@users.noreply.github.com>
2021-02-19 13:03:24 +0100

    Update data.R

--- data.R
+++ data.R
@@ -12,24 +12,43 @@ qplot(data = data, y = score, x = trial, geom = c("line", "point"),xlim = c(0, 5
   facet_wrap(~ participant)
 
 ## Score mean of each participant
-data %>%
+mean_score <- data %>%
   group_by(participant) %>%
   get_summary_stats(score, type = "mean_sd")
+
+bar_plot1 <- ggplot(data=mean_score, aes(x=participant, y=mean)) +
+  geom_bar(stat="identity")+
+  geom_text(aes(label=mean), vjust=-0.3, size=3.5)+
+  theme_minimal()
+
+barplot(mean_score$mean, names.arg=mean_score$participant)
 
 ## Score with 2*time_trial
 qplot(data = data, y = scoret2, x = trial, geom = c("line", "point"),xlim = c(0, 58)) + 
   facet_wrap(~ participant)
 
-data %>%
+mean_scoret2 <- data %>%
   group_by(participant) %>%
   get_summary_stats(scoret2, type = "mean_sd")
+
+bar_plot2 <-ggplot(data=mean_scoret2, aes(x=participant, y=mean)) +
+  geom_bar(stat="identity")+
+  geom_text(aes(label=mean), vjust=-0.3, size=3.5)+
+  theme_minimal()
 
 ## Score with 10*time_trial
 qplot(data = data, y = scoret3, x = trial, geom = c("line", "point"),xlim = c(0, 58)) + 
   facet_wrap(~ participant)
 
-data %>%
+mean_scoret3 <- data %>%
   group_by(participant) %>%
   get_summary_stats(scoret3, type = "mean_sd")
- 
+
+bar_plot3 <-ggplot(data=mean_scoret3, aes(x=participant, y=mean)) +
+  geom_bar(stat="identity")+
+  geom_text(aes(label=mean), vjust=-0.3, size=3.5)+
+  theme_minimal()
+
+require(gridExtra)
+grid.arrange(bar_plot1, bar_plot2,bar_plot3, ncol=3)
 
